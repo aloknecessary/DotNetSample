2e5f86b6699437ca676ed3b2a9d40a4d927b9362
Alok Ranjan Daftuar <adaftuar@rythmos.com>
2019-01-21 15:52:13 +0530

    added .net proj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to test project!");
+            Console.WriteLine("Creating new file..Please wait..");
+            var now = DateTime.UtcNow;
+
+            // Example #1: Write an array of strings to a file.
+            // Create a string array that consists of three lines.
+            string[] lines = { "First line", "Second line", "Third line" };
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            // Get the current directory.
+            string path = Directory.GetCurrentDirectory();
+            File.WriteAllLines(path + "\\WriteLines.txt", lines);
+
+            using (var file = new StreamWriter(@path + "\\WriteLines.txt", true))
+            {
+                file.WriteLine("Fourth line");
+                file.WriteLine("Timestamp: " + now);
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
